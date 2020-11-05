2c5f7c6158576056ed8067f3f49a869cd721f32b
Sultan Achmad <sultangendut@gmail.com>
2020-11-05 10:16:45 +0700

    feat(Add): add database connection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
+require('dotenv').config()
+import { connectDB } from './utils/database'
 import app from './app'
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log(
-    ` -> Microservice Account API started at http://localhost:${port}/`
-  )
-})
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log('-> Database Connected')
+      console.log(
+        ` -> Microservice Account API started at http://localhost:${port}/`
+      )
+    })
+  })
+  .catch((err: string) => {
+    console.log(err)
+  })
